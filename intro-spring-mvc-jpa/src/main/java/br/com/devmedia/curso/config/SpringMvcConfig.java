dd93213e4ec987f88a65b5aa2db86c2d70a2a408
Rafael <Rafael@DESKTOP-AT475HR>
2018-02-22 18:22:33 -0300

    Primeiro commit do projeto intro-spring-mvc-jpa

---
+++ intro-spring-mvc-jpa/src/main/java/br/com/devmedia/curso/config/SpringMvcConfig.java
@@ -0,0 +1,29 @@
+package br.com.devmedia.curso.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import br.com.devmedia.curso.web.conversor.TipoSexoConverter;
+
+@Configuration
+public class SpringMvcConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		registry.addConverter(new TipoSexoConverter());
+	}
+	
+}
