52fd524e6236d329c3b8c21ee88eb6fc8c1f11d4
Rafael <Rafael@DESKTOP-AT475HR>
2018-02-23 15:25:58 -0300

    Configuração da classe SpringJPAConfig

--- intro-spring-mvc-jpa/src/main/java/br/com/devmedia/curso/config/SpringJPAConfig.java
+++ intro-spring-mvc-jpa/src/main/java/br/com/devmedia/curso/config/SpringJPAConfig.java
@@ -1,9 +1,16 @@
 package br.com.devmedia.curso.config;
 
+import java.util.Properties;
+
+import javax.persistence.*;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 public class SpringJPAConfig {
 	 
@@ -20,6 +27,40 @@ public class SpringJPAConfig {
 	    return ds;
 	 }
 	
-	 	
+	 @Bean
+	 public EntityManagerFactory entityManagerFactory(DataSource dataSource) {
+	    
+		 LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();                  
+	    
+	    factory.setDataSource(dataSource);
+	    factory.setPackagesToScan("br.com.devmedia.curso.domain");
+	    factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+	    factory.setJpaProperties(jpaProperties());
+	    factory.afterPropertiesSet();
+	    
+	    return factory.getObject();
+	 }
+	 
+	 @Bean
+	  public JpaTransactionManager transactionManager(EntityManagerFactory factory) {
+	    
+		JpaTransactionManager tx = new JpaTransactionManager();
+	    
+		tx.setEntityManagerFactory(factory);
+	    tx.setJpaDialect(new HibernateJpaDialect());
+	  
+	  return tx;
+	 }
+	 
+	 
+	private Properties jpaProperties() {
+		Properties props = new Properties();
+		
+		props.setProperty("hibernate.show_sql", "true");
+		props.setProperty("hibernate.format_sql", "true");
+		props.setProperty("hibernate.hbm2ddl.auto", "update");
+		
+		return props;
+	}	
 	
 }
