23528f3435c861197a58711b1f259d09a32e39a1
Rafael <Rafael@DESKTOP-AT475HR>
2018-02-23 15:03:35 -0300

    Mapeamento da classe Usuario

--- intro-spring-mvc-jpa/src/main/java/br/com/devmedia/curso/domain/Usuario.java
+++ intro-spring-mvc-jpa/src/main/java/br/com/devmedia/curso/domain/Usuario.java
@@ -1,7 +1,9 @@
 package br.com.devmedia.curso.domain;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -9,22 +11,33 @@ import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
-public class Usuario {
+@Entity
+@Table( name = "usuarios" )
+public class Usuario implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(nullable = false, length = 50)
 	@NotBlank
 	@Size(min = 3, max = 50)
 	private String nome;
 	
+	@Column(nullable = false, length = 50)
 	@NotBlank
 	@Size(min = 3, max = 50, message = "Campo requerido entre {min} e {max} caracteres.")	
 	private String sobrenome;
 	
+	@Column( name = "data_nascimento" , nullable = false)
 	@NotNull(message = "O campo 'data de nascimento' é requerido.")
 	@DateTimeFormat(iso = ISO.DATE)
 	private LocalDate dtNascimento;
 	
+	@Column( name = "tipo_sexo", nullable = false )
+	@Enumerated(EnumType.STRING)
 	private TipoSexo sexo;
 	
 	public Usuario() {
@@ -98,6 +111,31 @@ public class Usuario {
 	@Override
 	public String toString() {
 		return "Usuario [id=" + id + ", nome=" + nome + ", sobrenome=" + sobrenome + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 	
