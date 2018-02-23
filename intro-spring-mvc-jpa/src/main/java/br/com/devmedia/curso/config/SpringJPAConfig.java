d4f1c877c7ca1158f4e10bfe4654be58c6b04dfc
Rafael <Rafael@DESKTOP-AT475HR>
2018-02-23 15:11:57 -0300

    Inclusão do método dataSource na classe SpringJPAConfig

--- intro-spring-mvc-jpa/src/main/java/br/com/devmedia/curso/config/SpringJPAConfig.java
+++ intro-spring-mvc-jpa/src/main/java/br/com/devmedia/curso/config/SpringJPAConfig.java
@@ -1,5 +1,25 @@
 package br.com.devmedia.curso.config;
 
-public class SpringJPAConfig {
+import javax.sql.DataSource;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+public class SpringJPAConfig {
+	 
+	 // A anotação @Bean transforma o método dataSource em um Bean gerenciado pelo Spring. Sendo obrigatório, pois sem ele o spring não vai conseguir gerenciar a conexão
+	 @Bean 
+     public DataSource dataSource() {
+	    DriverManagerDataSource ds = new DriverManagerDataSource();
+	
+	    ds.setDriverClassName("com.mysql.jdbc.Driver");
+	    ds.setUrl("jdbc:mysql://localhost:3316/test");
+	    ds.setUsername("root");
+	    ds.setPassword("root");
+	    
+	    return ds;
+	 }
+	
+	 	
+	
 }
